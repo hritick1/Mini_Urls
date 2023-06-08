7a83b12fba120f9fc7a8201df41e033e6f9dbba9
HRITICK KUMAR JHA <hjha9999@gmail.com>
2023-06-08 11:40:22 +0530

    Added cors

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ const app=express();
 const mongoose=require('mongoose');
 const dotenv=require('dotenv');
 const route=require('./routes');
+const cors=require('cors');
 
 //creating dotenv
 dotenv.config();
 
 //connecting to DB
 mongoose.connect(process.env.DB,{dbName:""});
-
+app.use(cors());
 app.use(express.json());
 
 
 app.use('/',route);
 
-app.listen(3000,()=>console.log('Server is Running'));
+app.listen(3001,()=>console.log('Server is Running'));
